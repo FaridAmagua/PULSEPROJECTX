afbc65d5623d36852cee3dc4fa3fd68334707726
FaridAmagua <faridamagua@live.com>
2024-08-31 13:01:11 +0200

    COLLAB NEW STYLE

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,18 @@ import Home from "./pages/Home";
 // import Contact from "./pages/Contact";
 import Egirls from "./pages/Egirls";
 import Collabs from './pages/Collabs';
+//Function
+import Scrolltotop from './components/Scrolltotop'
 
 function App() {
 
   return (
     <BrowserRouter basename="/">
-      <Navbar></Navbar>
+      <Scrolltotop/> {/* Este componente debe estar aquí */}
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/collabs" element={<Collabs />} />
-
         {/* <Route path="/contact" element={<Contact />} /> */}
         <Route path="/egirls" element={<Egirls />} />
       </Routes>
--- src/pages/Collabs.jsx
+++ src/pages/Collabs.jsx
@@ -2,22 +2,28 @@ import React from "react";
 
 export const Collabs = () => {
   return (
-    <div className="pt-[60px] bg-black text-white min-h-screen flex flex-col items-center justify-center text-center px-4">
-      <h1 className="text-3xl md:text-4xl font-bold mb-4">
-        ¡COLABORA CON NOSOSOTROS!
+    <div className="pt-[60px] bg-black text-white min-h-screen flex flex-col items-center justify-center text-center px-6">
+      {/* Título */}
+      <h1 className="text-4xl md:text-5xl font-bold mb-6 tracking-wide">
+        ¡COLABORA CON NOSOTROS!
       </h1>
-      <p className="mb-4 text-lg md:text-xl">
-        ¿Eres influencer, deportista o una personalidad conocida? ¡Estamos
-        buscando colaboradores como tú! Explora nuestras exclusivas
+
+      {/* Texto descriptivo */}
+      <p className="mb-6 text-lg md:text-xl leading-relaxed max-w-2xl">
+        ¿Eres influencer, deportista o una personalidad conocida?
+      </p>
+
+      {/* Descripción detallada */}
+      <p className="text-md md:text-lg mb-8 text-justify max-w-lg px-4 md:px-0">
+        Estamos buscando colaboradores como tú! Explora nuestras exclusivas
         oportunidades: botellas personalizadas, invitaciones VIP y servicios
         únicos diseñados solo para ti.
       </p>
-      <p className="mb-6 text-lg md:text-xl">
-        ¡Haz clic en el botón y escríbenos para descubrir más!
-      </p>
+
+      {/* Botón de contacto */}
       <a
         href="https://wa.me/message/SG7R44Q336VZO1"
-        className="bg-white text-black py-3 px-8 rounded-lg hover:bg-gray-200 transition-colors text-lg font-semibold"
+        className="bg-gradient-to-r from-pink-500 to-red-500 text-white py-2 px-4 rounded-md hover:bg-pink-600 hover:from-pink-600 hover:to-red-600 transition-colors duration-300 text-xl font-semibold"
       >
         Contáctanos
       </a>
